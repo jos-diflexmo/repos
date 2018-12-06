e585516fd211a3a013f0287be6c33d96c44d33fe
Joseph Desmet <joseph@diflexmo.be>
2018-12-06 18:28:33 +0100

    Add project files.

---
+++ WebApplication6/WebApplication6/Controllers/Test/Ping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace WebApplication6.Controllers.Test
+{
+    public class Ping : IRequest<string>
+    {
+    }
+
+    public class PingHandler : RequestHandler<Ping, string>
+    {
+        protected override string Handle(Ping request)
+        {
+            return "Pong";
+        }
+    }
+}
---
+++ WebApplication6/WebApplication6/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication6.Controllers.Test;
+
+namespace WebApplication6.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public TestController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Ping()
+        {
+            var response = await _mediator.Send(new Ping());
+            Debug.WriteLine(response); // "Pong"
+
+            return View();
+        }
+    }
+}
